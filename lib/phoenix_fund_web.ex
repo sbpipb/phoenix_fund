2967c989099f8e2cf2174b29837a9b77f3a72787
sbpipb <sbpipb>
2023-05-26 05:00:36 +0800

    post schema and helper

--- lib/phoenix_fund_web.ex
+++ lib/phoenix_fund_web.ex
@@ -92,6 +92,7 @@ defmodule PhoenixFundWeb do
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
       import Phoenix.LiveView.Helpers
 
+      import PhoenixFundWeb.LiveHelpers
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
 
